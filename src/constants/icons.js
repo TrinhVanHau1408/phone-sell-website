edc67b8b69694da8f78d7b85b42cbe6d00427957
TrinhVanHau1408 <hauaclas@gmail.com>
2024-03-14 21:43:39 +0700

    constants file

---
+++ src/constants/icons.js
@@ -0,0 +1,6 @@
+import google from '../../src/assets/icons/google.png'
+import facebook from '../../src/assets/icons/facebook.png'
+export const icons = {
+    'google-icon': google,
+    'facebook-icon': facebook,
+}
